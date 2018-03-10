ee9f694adbda8361fe55a8f7be6001fc14caa53b
smallbee <smallbee3@gmail.com>
2018-03-10 21:30:48 +0900

    Let's make melon again!

---
+++ app/config/settings/local.py
@@ -0,0 +1,22 @@
+from .base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+INSTALLED_APPS += [
+    'django_extensions',
+]
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+
